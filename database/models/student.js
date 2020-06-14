182c5c67787009c794e8ae26ec27a1bba1841b9a
Manahil Sher <manahil.sher@gmail.com>
2020-06-13 22:58:04 -0400

    unique email

--- database/models/student.js
+++ database/models/student.js
@@ -4,10 +4,16 @@ const db = require('../db');
 const Student = db.define('student', {
   firstName: { type: Sequelize.STRING, allowNull: false },
   lastName: { type: Sequelize.STRING, allowNull: false },
-  email: { type: Sequelize.STRING, allowNull: false },
+  email: {
+    type: Sequelize.STRING,
+    allowNull: false,
+    isEmail: true,
+    unique: true
+  },
   imageUrl: {
     type: Sequelize.STRING,
-    defaultValue: 'https://via.placeholder.com/480x240?text=Placeholder'
+    defaultValue: 'https://via.placeholder.com/480x240?text=Placeholder',
+    allowNull: false
   },
   gpa: { type: Sequelize.DOUBLE, defaultValue: 0.0, allowNull: false }
 });
